53e16b1a2f52bb4345f307f3641433bb11d4b9bf
JunpeiL <junpei.liang@gmail.com>
2017-12-27 15:31:10 -0800

    third

--- coder-swag/coder-swag/Controller/CategoriesViewController.swift
+++ coder-swag/coder-swag/Controller/CategoriesViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CategoriesViewController.swift
 //  coder-swag
 //
 //  Created by offline on 26/12/2017.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CategoriesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ coder-swag/coder-swag/View/CategoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryTableViewCell.swift
+//  coder-swag
+//
+//  Created by offline on 26/12/2017.
+//  Copyright © 2017 JoP. All rights reserved.
+//
+
+import UIKit
+
+class CategoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
